7314f1d113924ea9c6fcdc63648082f381fd52fe
emanuelvasconnobre <emanuelvanobre@gmail.com>
2024-03-11 19:41:13 -0300

    Initial Commit

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "project" {
+  description = "ID do projeto no GCP"
+}
+
+variable "region" {
+  description = "Região do GCP onde o cluster será criado"
+  default = "southamerica-east1"
+}
+
+variable "credentials_file" {
+  description = "Caminho para o arquivo de credenciais do GCP"
+  default = "./gcp-credentials.private.json"
+}
